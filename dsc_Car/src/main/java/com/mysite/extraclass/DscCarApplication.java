6531ba24b3f9a7c9bc9709ad5ff6b6f4e33e30ad
Gun-Hee-SON <songunhee5426@gmail.com>
2024-08-16 17:41:40 +0900

    기초공사

---
+++ dsc_Car/src/main/java/com/mysite/extraclass/DscCarApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.extraclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscCarApplication.class, args);
+	}
+
+}
